cb53d108c9edb9b6ee261aa63744d7c05f850d13
deandejong89-png <deandejong89@gmail.com>
2026-09-08 14:05:41 +0200

    mozaiek

--- Week 1 - Hello World/sketch.js
+++ Week 1 - Hello World/sketch.js
@@ -1,7 +1,66 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(750, 500);
 }
 
 function draw() {
   background(220);
+  fill(0);
+  text("1. Dean de jong, ", 50, 50);
+  text("2.", 50, 100);
+  fill(255,0,0);
+  strokeWeight(0);
+  rect(75,100,100,25 );
+  fill(255);
+  rect(75,125,100,25)
+  fill(0,0,255)
+  rect(75,150,100,25);
+  fill(0);
+  text("3.", 50, 200);
+  fill(255);
+  strokeWeight(5);
+  rect(70,225,90,90);
+  strokeWeight(0)
+  fill(0);
+  rect(70,225,30,30);
+  fill(0);
+  rect(130, 225,30,30);
+  fill(0);
+  rect(100, 255, 30, 30);
+  fill(0);
+  rect(70,285,30,30);
+  fill(0);
+  rect(130,285,30,30);
+  text("4.",50,350);
+  strokeWeight(3.5);
+  fill(220);
+  rect(75, 375, 50,50);
+  fill(0);
+  line(75, 375, 100, 350);
+  line(125,375,100, 350);
+  fill(0);
+  strokeWeight(0);
+  text("5.",250, 50);
+  fill(100);
+  rect(275,50,50,175);
+  fill(225,0,0);
+  circle(300,75,35);
+  fill(255,165,0);
+  circle(300,125,35);
+  fill(0,128,0);
+  circle(300,175,35);
+  fill(150);
+  rect(287.5,225,25,60);
+  fill(0);
+  text("6.", 250,300);
+  fill(255);
+  strokeWeight(5);
+  rect(275, 300, 100, 100,10);
+  fill(0);
+  circle(300,325,20);
+  circle(325,350,20);
+  circle(350,375,20);
+    
+  
+        
+
 }
